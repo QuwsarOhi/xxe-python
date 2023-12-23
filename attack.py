197951f1a6cb92ae9039edfa94f967c03350f5dc
Protozet <63610185+Protozet@users.noreply.github.com>
2023-12-22 19:17:47 -0700

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,7 +1,7 @@
-#  This is the attack code
-#  The attacker would send the server a vulnerable xml script
-#  In this example, the xxe is designed to read a "secret" file
-#  of which the attacker should not have access to
+# This is a demonstration of an XXE (XML External Entity) attack.
+# The attacker crafts a malicious XML document to exploit poorly configured XML parsers.
+# In this example, the XXE is designed to read a "secret" file,
+# which the attacker should not have access to.
 
 import subprocess
 
@@ -22,6 +22,12 @@ xml = '''\
 </data>
 '''
 
-#  Send xml to server.py and print output
+# Send the malicious XML to the server and capture the output.
+# The vulnerability lies in the server's XML parser, which processes external entities.
+# Developers may overlook disabling external entities in XML parsers, leading to this vulnerability.
+
 server_output = subprocess.check_output(["python3", "server.py", xml])
 print(server_output.decode())
+
+# Minimal fix: The server should be configured to disable the processing of external entities in XML.
+# This can be done by appropriately setting the parser configuration or using a library that is secure by default.
